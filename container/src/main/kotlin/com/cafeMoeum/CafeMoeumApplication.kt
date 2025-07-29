b3484def8d88733e1bd6495134a346725e8b0833
Seungkyu-Han <trust1204@gmail.com>
2025-07-29 18:00:31 +0900

    feat: container spring application

---
+++ container/src/main/kotlin/com/cafeMoeum/CafeMoeumApplication.kt
@@ -0,0 +1,11 @@
+package com.cafeMoeum
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CafeMoeumApplication
+
+fun main(args: Array<String>) {
+    runApplication<CafeMoeumApplication>(*args)
+}
