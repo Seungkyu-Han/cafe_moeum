7dbcb776bf3c09b826c03f8c6269e2cdf972986b
Seungkyu-Han <trust1204@gmail.com>
2025-08-01 08:17:03 +0900

    docs: update menu request dto swagger docs

--- sync/sync-presentation/src/main/kotlin/com/cafeMoeum/dto/UpdateMenuReq.kt
+++ sync/sync-presentation/src/main/kotlin/com/cafeMoeum/dto/UpdateMenuReq.kt
@@ -2,8 +2,12 @@ package com.cafeMoeum.dto
 
 import com.cafeMoeum.enums.CafeEnum
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
 
+@Schema(name = "카페 메뉴 크롤러 DTO", description = "해당 카페의 메뉴를 크롤링해서 업데이트 해줍니다.")
 data class UpdateMenuReq(
+
     @JsonProperty("cafe")
+    @Schema(description = "카페의 종류")
     val cafe: CafeEnum,
 )
