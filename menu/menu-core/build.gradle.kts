6bcc3cad921f477cf2a0002fd660afa560ba4536
Seungkyu-Han <trust1204@gmail.com>
2025-07-31 22:41:50 +0900

    feat: create menu module

---
+++ menu/menu-core/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.4"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "1.9.25"
+}
+
+group = "com"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation(project(":common"))
+    implementation(project(":persistence"))
+    implementation(project(":menu:menu-common"))
+
+    implementation("org.springframework:spring-tx:6.2.9")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
