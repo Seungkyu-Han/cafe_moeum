dac3ebde48dc095f576ffa435340c84e4b818dba
Seungkyu-Han <trust1204@gmail.com>
2025-08-01 12:16:23 +0900

    docs: menu module swagger docs

--- menu/menu-common/src/main/kotlin/com/cafeMoeum/dto/CafeInfoRes.kt
+++ menu/menu-common/src/main/kotlin/com/cafeMoeum/dto/CafeInfoRes.kt
@@ -1,5 +1,9 @@
 package com.cafeMoeum.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(name = "카페 메뉴 및 카테고리 응답 DTO", description = "해당 카페의 메뉴 및 카테고리를 조회해줍니다.")
 data class CafeInfoRes(
+
     val categories: List<CategoryRes>
 )
--- menu/menu-common/src/main/kotlin/com/cafeMoeum/dto/MenuRes.kt
+++ menu/menu-common/src/main/kotlin/com/cafeMoeum/dto/MenuRes.kt
@@ -1,8 +1,19 @@
 package com.cafeMoeum.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "카테고리")
 data class MenuRes(
+
+    @Schema(description = "메뉴 한글 명")
     val nameKr: String,
+
+    @Schema(description = "메뉴 영문 명")
     val nameEn: String,
+
+    @Schema(description = "메뉴 정렬 순서")
     val sortOrder: Int,
+
+    @Schema(description = "메뉴 이미지")
     val img: String
 )
