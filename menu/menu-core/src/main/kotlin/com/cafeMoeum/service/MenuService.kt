1fa26ed6a46e07f3e7db18c7b5218442525e9523
Seungkyu-Han <trust1204@gmail.com>
2025-08-08 12:20:40 +0900

    feat: menu search api

--- menu/menu-core/src/main/kotlin/com/cafeMoeum/service/MenuService.kt
+++ menu/menu-core/src/main/kotlin/com/cafeMoeum/service/MenuService.kt
@@ -1,8 +1,11 @@
 package com.cafeMoeum.service
 
 import com.cafeMoeum.dto.CategoryMenuRes
+import com.cafeMoeum.dto.SearchMenuRes
 
 interface MenuService {
 
     fun getCafeInfo(cafeType: String): List<CategoryMenuRes>
+
+    fun getSearchMenu(keyword: String): List<SearchMenuRes>
 }
--- menu/menu-presentation/src/main/kotlin/com/cafeMoeum/controller/MenuController.kt
+++ menu/menu-presentation/src/main/kotlin/com/cafeMoeum/controller/MenuController.kt
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/menus")
@@ -26,5 +23,14 @@ class MenuController(
     )
     fun getMenus(@PathVariable cafeType: String): ResponseEntity<List<CategoryMenuRes>> {
         return ResponseEntity.ok(menuService.getCafeInfo(cafeType))
+    }
+
+    @GetMapping("/search")
+    @Operation(summary = "키워드로 메뉴들을 검색합니다")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "검색 성공")
+    )
+    fun getSearchMenu(@RequestParam keyword: String): ResponseEntity<List<CategoryMenuRes>> {
+        return ResponseEntity.ok(menuService.getCafeInfo(keyword))
     }
 }
