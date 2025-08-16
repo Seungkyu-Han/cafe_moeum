1fc19bcddfd98efdfd0766a365e15910d70a9bc3
Seungkyu-Han <trust1204@gmail.com>
2025-08-16 16:43:21 +0900

    docs: hit count dto swagger docs

--- hit/hit-presentation/src/main/kotlin/com/cafeMoeum/controller/HitController.kt
+++ hit/hit-presentation/src/main/kotlin/com/cafeMoeum/controller/HitController.kt
@@ -5,6 +5,7 @@ import com.cafeMoeum.service.HitReadService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/hit")
+@Tag(name = "조회수 API", description = "방문자의 수를 조회합니다.")
 class HitController(
     private val hitReadService: HitReadService
 ) {
