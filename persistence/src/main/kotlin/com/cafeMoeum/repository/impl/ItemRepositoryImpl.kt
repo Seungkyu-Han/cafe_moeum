d8e5a8c9a1b3c1f01f6a8e40e71da8c09583bc0b
한승규 <trust1204@sampyo.co.kr>
2025-09-03 15:26:30 +0900

    feat: add delete by cart method in item repository

--- persistence/src/main/kotlin/com/cafeMoeum/repository/impl/ItemRepositoryImpl.kt
+++ persistence/src/main/kotlin/com/cafeMoeum/repository/impl/ItemRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.cafeMoeum.repository.impl
 
+import com.cafeMoeum.entities.Cart
 import com.cafeMoeum.repository.ItemRepository
 import com.cafeMoeum.repository.jpa.ItemJpaRepository
 import org.springframework.stereotype.Repository
@@ -8,4 +9,8 @@ import org.springframework.stereotype.Repository
 class ItemRepositoryImpl (
     private val itemJpaRepository: ItemJpaRepository
 ): ItemRepository {
+
+    override fun deleteByCart(cart: Cart) {
+        itemJpaRepository.deleteByCart(cart)
+    }
 }
